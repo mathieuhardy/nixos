2b023b9c6bbd07b167cfd4249bae610759b0cbd2
Mathieu Hardy <mhardy2008@gmail.com>
2026-02-21 22:16:28 +0100

    Cleaning

--- system/hyprland/default.nix
+++ system/hyprland/default.nix
@@ -18,20 +18,8 @@
   # TODO: to be sorted
   security.pam.services.hyprlock = { };
 
-  environment.systemPackages = with pkgs; [
-    # ddcutil # For brightnessctl to work on external screen
-  ];
-
   # services.xdg-desktop-portal = {
   #   enable = true;
   #   extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
   # };
-
-  # Pour accéder à l'écran sans root (brightnessctl)
-  # hardware.i2c.enable = true;
-  # users.users.mhardy.extraGroups = [ "i2c" ];
-  # ddcutil setvcp 10 70  # luminosité à 70%
-  # ddcutil getvcp 10     # valeur actuelle
-  # bind = , XF86MonBrightnessUp, exec, ddcutil setvcp 10 + 10
-  # bind = , XF86MonBrightnessDown, exec, ddcutil setvcp 10 - 10
 }
