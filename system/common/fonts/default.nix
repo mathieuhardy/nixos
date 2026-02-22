e800136a69138649c74ccbcb6c235cb981f88351
Mathieu Hardy <mhardy2008@gmail.com>
2026-02-22 13:56:23 +0100

    Emoji font

--- system/common/fonts/default.nix
+++ system/common/fonts/default.nix
@@ -9,12 +9,13 @@
     packages = with pkgs; [
       dejavu_fonts
       nerd-fonts.commit-mono
-      source-code-pro
+      noto-fonts-color-emoji
     ];
 
     fontconfig = {
       defaultFonts = {
         monospace = [ "DejaVu Sans Mono" ];
+        emoji = [ "Noto Color Emoji" ];
       };
     };
   };
