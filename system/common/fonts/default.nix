2eac04ea06d7af8ea87e5e3fa8ca5e422c0cb16e
Mathieu Hardy <mhardy2008@gmail.com>
2026-02-22 11:59:26 +0100

    Add default font

--- system/common/fonts/default.nix
+++ system/common/fonts/default.nix
@@ -5,9 +5,17 @@
   # Fonts
   # ────────────────────────────────────────────────────────────────────────────
 
-  fonts.packages = with pkgs; [
-    dejavu_fonts
-    nerd-fonts.commit-mono
-    source-code-pro
-  ];
+  fonts = {
+    packages = with pkgs; [
+      dejavu_fonts
+      nerd-fonts.commit-mono
+      source-code-pro
+    ];
+
+    fontconfig = {
+      defaultFonts = {
+        monospace = [ "DejaVu Sans Mono" ];
+      };
+    };
+  };
 }
