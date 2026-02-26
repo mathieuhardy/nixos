fc5f80b08a74983025295d9a2f3c3f81f9d5ab81
Mathieu Hardy <mhardy2008@gmail.com>
2026-02-26 22:13:38 +0100

    Force SWT to GTK3

--- home-manager/theme.nix
+++ home-manager/theme.nix
@@ -54,16 +54,18 @@ in
   #   gtk-theme = "catppuccin-frappe-mauve-normal"; # Some apps may use this
   # };
 
-  # TODO: remove
-  # home.sessionVariables = {
-  #   GTK_THEME = "catppuccin-frappe-mauve-normal";
-  #
-  #   # Force dark mode in libadwaita/GTK4
-  #   ADW_DISABLE_PORTAL = "1";
-  # };
+  home.sessionVariables = {
+    # Force SWT applications (dbeaver, Eclipse, ...) to use GTK3 instead of GTK2
+    SWT_GTK3 = "1";
+
+    # Theme for GTK
+    GTK_THEME = "catppuccin-frappe-mauve-standard:dark";
+
+    # # Force dark mode in libadwaita/GTK4
+    # ADW_DISABLE_PORTAL = "1";
+  };
 
   # Force dark mode for GTK4 using settings.ini
-  # TODO: remove if all GTK apps are well configured
   xdg.configFile."gtk-4.0/settings.ini".text = ''
     [Settings]
     gtk-application-prefer-dark-theme=true
