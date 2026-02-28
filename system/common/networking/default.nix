48577c73da8a9898f7a2d4c62b0319e0d1a1928c
Mathieu Hardy <mhardy2008@gmail.com>
2026-02-28 23:17:30 +0100

    Enable wifi and impala

--- system/common/networking/default.nix
+++ system/common/networking/default.nix
@@ -15,15 +15,31 @@
     # NetworkManager
     networkmanager = {
       enable = true;
+
       wifi.backend = "iwd";
+
+      # Force to handle all interfaces
+      unmanaged = [ ];
     };
 
-    # Wifi
-    wireless.iwd.enable = true;
+    # Wifi (enable iwd and disable wpa_supplicant)
+    wireless.iwd = {
+      enable = true;
+
+      settings = {
+        General = {
+          # NM handles DHCP
+          EnableNetworkConfiguration = false;
+        };
+      };
+    };
+
+    wireless.enable = false;
 
     # Firewall
     firewall = {
       enable = true;
+
       allowPing = true;
     };
   };
