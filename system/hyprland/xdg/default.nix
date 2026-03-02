590e83aa17eaae66ec1f6afe680af1a01e33428b
Mathieu Hardy <mhardy2008@gmail.com>
2026-03-02 21:33:46 +0100

    Add portal for gnome

--- system/hyprland/xdg/default.nix
+++ system/hyprland/xdg/default.nix
@@ -11,14 +11,20 @@
     extraPortals = with pkgs; [
       xdg-desktop-portal-hyprland
       xdg-desktop-portal-gtk
+      xdg-desktop-portal-gnome
     ];
 
     config = {
       hyprland.default = [
         "hyprland"
         "gtk"
+        "gnome"
       ];
-      common.default = [ "gtk" ];
+
+      common.default = [
+        "gtk"
+        "gnome"
+      ];
     };
   };
 }
