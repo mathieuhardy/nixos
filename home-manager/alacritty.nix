1f507e18e609bbdf3b1ab245d10c3fa0a9d61a6d
Mathieu Hardy <mhardy2008@gmail.com>
2026-03-08 13:39:19 +0100

    Move alacritty here

--- home-manager/alacritty.nix
+++ home-manager/alacritty.nix
@@ -7,5 +7,5 @@
   # ────────────────────────────────────────────────────────────────────────────
 
   xdg.configFile."alacritty".source =
-    config.lib.file.mkOutOfStoreSymlink "/home/${osConfig.settings.userLogin}/${osConfig.settings.repos}/alacritty-config";
+    config.lib.file.mkOutOfStoreSymlink "/home/${osConfig.settings.userLogin}/${osConfig.settings.repos}/nixos/home-manager/configs/alacritty";
 }
